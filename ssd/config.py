d714e07c5859fa082b7ca96bbc97d945d9f926d1
PiotrJZielinski <piotrekzie100@gmail.com>
2020-05-21 14:53:39 +0200

    Add backbone and box predictor name parameter to config

--- ssd/config.py
+++ ssd/config.py
@@ -5,6 +5,9 @@ from typing import Optional
 
 from yacs.config import CfgNode
 
+from ssd.modeling.backbones import backbones
+from ssd.modeling.box_predictors import box_predictors
+
 _C = CfgNode()
 
 # data config
@@ -18,9 +21,19 @@ _C.MODEL.BATCH_NORM = True
 _C.MODEL.PRETRAINED_URL = (
     "https://s3.amazonaws.com/amdegroot-models/vgg16_reducedfc.pth"
 )
+_C.MODEL.BACKBONE = "VGG300"
+_C.MODEL.BOX_PREDICTOR = "SSD"
 
 
 logger = logging.getLogger(__name__)
+
+
+def verify_config(config: CfgNode):
+    """Verify if chosen options are correct."""
+    if config.MODEL.BACKBONE not in backbones:
+        raise NameError("Backbone %s is not available", config.MODEL.BACKBONE)
+    if config.MODEL.BOX_PREDICTOR not in box_predictors:
+        raise NameError("Box predictor %s is not available", config.MODEL.BOX_PREDICTOR)
 
 
 def get_config(config_file: Optional[Path] = None, **kwargs):
@@ -33,4 +46,5 @@ def get_config(config_file: Optional[Path] = None, **kwargs):
             logger.warning("File %s does not exist.", str(config_file))
     config.update(**kwargs)
     config.freeze()
+    verify_config(config)
     return config
