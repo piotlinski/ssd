58d68734e077299277946bf3d50ce682ce7be206
Piotr Zieliński <piotrekzie100@gmail.com>
2020-05-26 14:17:41 +0200

    Feature/dataset stats (#10)
    * Add calculating dataset stats
    
    * Add CLI command to calculate dataset stats

--- tests/test_dataset_base.py
+++ tests/test_dataset_base.py
@@ -1,5 +1,9 @@
 """Tests for base dataset class."""
 from pathlib import Path
+from unittest.mock import patch
+
+import pytest
+import torch
 
 from ssd.data.datasets.base import BaseDataset
 
@@ -11,3 +15,16 @@ def test_base_dataset_params():
     assert ds.data_dir == Path(path)
     assert ds.data_transform is None
     assert ds.target_transform is None
+
+
+@patch("ssd.data.datasets.base.BaseDataset.__len__", return_value=10)
+@patch("ssd.data.datasets.base.BaseDataset.__getitem__")
+@pytest.mark.parametrize("channels", (1, 3))
+def test_calculating_dataset_stats(getitem_mock, _len_mock, channels):
+    """Verify if dataset stats are calculated properly."""
+    getitem_mock.return_value = torch.ones((channels, 5, 5)), None, None
+    ds = BaseDataset(".")
+    print(ds[0])
+    pixel_mean, pixel_std = ds.pixel_mean_std()
+    assert pixel_mean == channels * (1,)
+    assert pixel_std == channels * (0,)
