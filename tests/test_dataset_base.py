879ee2dca411c2c8b5ed0549c89b27054cbac453
Piotr Zieliński <piotrekzie100@gmail.com>
2020-07-01 15:59:13 +0200

    Fix onehot for single class

--- tests/test_dataset_base.py
+++ tests/test_dataset_base.py
@@ -32,6 +32,7 @@ def test_calculating_dataset_stats(getitem_mock, _len_mock, channels):
 @pytest.mark.parametrize(
     "flat, encoded, n_classes",
     [
+        (torch.tensor([[0, 0, 1]]), torch.tensor([[[0.0], [0.0], [1.0]]]), 1),
         (
             torch.tensor([[0, 1, 2]]),
             torch.tensor([[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]]),
