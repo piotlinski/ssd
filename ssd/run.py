8f2ba5823c4ba32cd958ea815faa61d4dc4665de
Piotr Zieliński <piotrekzie100@gmail.com>
2020-07-28 23:22:49 +0200

    Rollback passing dataloader (#40)

--- ssd/run.py
+++ ssd/run.py
@@ -220,7 +220,6 @@ class Runner:
                                             self.config, labels=labels.cpu()
                                         ),
                                         gt_bbox_pred=locations.cpu(),
-                                        data_loader=data_loader,
                                     ),
                                     global_step=global_step,
                                 )
@@ -297,7 +296,6 @@ class Runner:
                     pred_bbox_pred=bbox_pred.cpu(),
                     gt_cls_logits=onehot_labels(self.config, labels=labels.cpu()),
                     gt_bbox_pred=locations.cpu(),
-                    data_loader=data_loader,
                 ),
                 global_step=global_step,
             )
