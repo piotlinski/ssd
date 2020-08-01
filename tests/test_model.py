4ee0c12000423f4d667aee3077a83d3c517f8fd1
Piotr Zieliński <piotrekzie100@gmail.com>
2020-08-01 21:00:30 +0200

    Runner bugfixes (#42)
    * Fix downloading the dataset and add CLI entry
    
    * Move onehot labels to appropriate deice
    
    * Move denormalization to appropriate device
    
    * Use CUDA for model and handle NMS error
    
    * Use CUDA for evaluation and add progress bar
    
    * Visualize once per set number of epochs
    
    * Fix tests with default confidence threshold

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,7 +4,7 @@ import torch
 
 from ssd.modeling.backbones import backbones
 from ssd.modeling.box_predictors import box_predictors
-from ssd.modeling.model import SSD, non_max_suppression, process_model_output
+from ssd.modeling.model import SSD, process_model_output
 
 
 def test_model_setup(sample_config):
@@ -26,26 +26,6 @@ def test_model_forward(sample_config):
     assert bbox_pred.shape[0] == 1
     assert bbox_pred.shape[2] == 4
     assert cls_logits.shape[1] == bbox_pred.shape[1]
-
-
-@pytest.mark.parametrize("n_boxes", [1, 2, 3, 4])
-def test_nms(n_boxes):
-    """Test non-max suppression return format."""
-    boxes = torch.rand((n_boxes, 4))
-    scores = torch.rand(n_boxes)
-    indices = torch.rand(n_boxes)
-    threshold = 0.5
-    nms = non_max_suppression(
-        boxes=boxes, scores=scores, indices=indices, threshold=threshold
-    )
-    assert 0 <= nms.shape[0] <= n_boxes
-
-
-def test_nms_empty():
-    """Test non-max suppression for empty tensor."""
-    empty = torch.empty(0)
-    nms = non_max_suppression(boxes=empty, scores=empty, indices=empty, threshold=0.0)
-    assert nms.numel() == 0
 
 
 @pytest.mark.parametrize("n_predictions", [0, 1, 2])
