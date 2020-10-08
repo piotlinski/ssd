f8f01c723720967e9653c416d8312f080a8eb40c
Piotr Zieliński <piotrekzie100@gmail.com>
2020-10-08 21:32:48 +0200

    VGG lite backbone (#60)
    * Fix multi-core tests
    
    * Add VGGLite backbone
    
    * Refactor backbones to use VGG11 and VGG16 only

--- pyssd/modeling/backbones/__init__.py
+++ pyssd/modeling/backbones/__init__.py
@@ -1,7 +1,10 @@
-from typing import Dict
+from typing import Dict, Type
 
 import torch.nn as nn
 
-from pyssd.modeling.backbones.vgg import VGG300, VGG512
+from pyssd.modeling.backbones.vgg import VGG11, VGG16
 
-backbones: Dict[str, nn.Module] = {"VGG300": VGG300, "VGG512": VGG512}
+backbones: Dict[str, Type[nn.Module]] = {
+    "VGG11": VGG11,
+    "VGG16": VGG16,
+}
