9968c47c36658648279e2905dc4d37ca93860dbc
Piotr Zieliński <piotrekzie100@gmail.com>
2020-11-03 23:51:43 +0100

    MobileNetV2 backbone (#69)
    * Add mobilenetv2 backbone
    
    * Fix dev dockerfile
    
    * Add ipython to dev deps
    
    * Add working MobileNetV2 backbone with tests
    
    * Fix prod image

--- pyssd/modeling/backbones/__init__.py
+++ pyssd/modeling/backbones/__init__.py
@@ -2,9 +2,11 @@ from typing import Dict, Type
 
 import torch.nn as nn
 
+from pyssd.modeling.backbones.mobilenetv2 import MobileNetV2
 from pyssd.modeling.backbones.vgg import VGG11, VGG16
 
 backbones: Dict[str, Type[nn.Module]] = {
     "VGG11": VGG11,
     "VGG16": VGG16,
+    "mobilenetv2": MobileNetV2,
 }
