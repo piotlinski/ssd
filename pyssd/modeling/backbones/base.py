3589b23a4be09d2ae70aca29ae7cb61c360716b2
Piotr Zieliński <piotrekzie100@gmail.com>
2020-11-07 11:53:40 +0100

    Refactor backbones not to use config

--- pyssd/modeling/backbones/base.py
+++ pyssd/modeling/backbones/base.py
@@ -3,20 +3,43 @@ from typing import List, Tuple
 
 import torch
 import torch.nn as nn
-from yacs.config import CfgNode
 
 
 class BaseBackbone(nn.Module):
-    def __init__(self, config: CfgNode, out_channels: List[int]):
+    def __init__(
+        self,
+        out_channels: List[int],
+        feature_maps: List[int],
+        min_sizes: List[float],
+        max_sizes: List[float],
+        strides: List[int],
+        aspect_ratios: List[Tuple[int, ...]],
+        use_pretrained: bool,
+    ):
         """
-        :param config: SSD config
-        :param out_channels: output channels of the backbone
+        :param out_channels: backbone output channels
+        :param feature_maps: number of features in each output map
+        :param min_sizes: minimum object size in each feature map
+        :param max_sizes: maximum object size in each feature map
+        :param strides: stride in each feature map
+        :param aspect_ratios: additional rectangular boxes in each feature map
+        :param use_pretrained: use pretrained backbone
         """
         super().__init__()
-        self.config = config
+
+        self.out_channels = out_channels
+        self.feature_maps = feature_maps
+        self.min_sizes = min_sizes
+        self.max_sizes = max_sizes
+        self.strides = strides
+        self.aspect_ratios = aspect_ratios
+        self.boxes_per_loc = [
+            2 + 2 * len(aspect_ratio_tuple) for aspect_ratio_tuple in self.aspect_ratios
+        ]
+        self.use_pretrained = use_pretrained
+
         self.backbone = self._build_backbone()
         self.extras = self._build_extras()
-        self.out_channels = out_channels
         self.init_extras()
 
     def init_extras(self):
