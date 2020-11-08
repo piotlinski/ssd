47847ad7e16a62fd2bd95c33784a2021bcf5b4d3
Piotr Zieliński <piotrekzie100@gmail.com>
2020-11-08 18:48:42 +0100

    Add option to load config from yaml

--- pyssd/main.py
+++ pyssd/main.py
@@ -1,9 +1,20 @@
 """Main function for SSD training."""
+import argparse
 from argparse import ArgumentParser
 
+import yaml
 from pytorch_lightning import Trainer
 
 from pyssd.modeling.model import SSD
+
+
+class LoadFromYaml(argparse.Action):
+    """Load arguments from YAML file."""
+
+    def __call__(self, parser: ArgumentParser, namespace, values, option_string=None):
+        with values as fp:
+            for key, value in yaml.load(fp, Loader=yaml.Loader).items():
+                setattr(namespace, key, value)
 
 
 def main(hparams):
@@ -18,6 +29,13 @@ def main(hparams):
 def cli():
     """SSD CLI with argparse."""
     parser = ArgumentParser()
+    parser.add_argument(
+        "-f",
+        "--file",
+        type=open,
+        action=LoadFromYaml,
+        help="YAML file with arguments to use",
+    )
     parser = SSD.add_model_specific_args(parser)
     parser = Trainer.add_argparse_args(parser)
     args = parser.parse_args()
